a638fc80d64f676199c32b2af388fd59586757e0
datngusi <thainga0909@gmail.com>
2019-10-19 10:50:15 +0700

    first commit

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class login extends Component {
+  render() {
+    return <div>
+        <h1>Home page</h1>
+    </div>;
+  }
+}
+
+export default login;
