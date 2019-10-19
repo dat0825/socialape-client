7be29f1dd2315af319b43b465ed6c1765b99345f
datngusi <thainga0909@gmail.com>
2019-10-19 11:51:30 +0700

    4h44p

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class home extends Component {
   render() {
-    return <div>
+    return (
+      <div>
         <h1>Home page</h1>
-    </div>;
+      </div>
+    );
   }
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class login extends Component {
   render() {
-    return <div>
-        <h1>Home page</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    );
   }
 }
 
